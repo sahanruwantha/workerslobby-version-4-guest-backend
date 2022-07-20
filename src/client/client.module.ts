993ddad20eb5e584e7844d9f68dafdb4e69fe7af
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-07-20 23:15:54 +0530

    added modules

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
+import { ClientController } from './client.controller';
 
 @Module({
   providers: [ClientService],
--- src/engineer/engineer.module.ts
+++ src/engineer/engineer.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { EngineerService } from './engineer.service';
-import { EngineerService } from './engineer.service';
 
 @Module({
   providers: [EngineerService]
