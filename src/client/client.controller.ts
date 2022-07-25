9187cab8b2ff47f293fb6005704ab1d7aff15110
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-07-25 10:44:36 +0530

    updated modules

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -3,4 +3,6 @@ import { CreateTicketDto } from '../ticket/ticket-dto/create-ticket-dto';
 import { TicketService } from '../ticket/ticket.service';
 
 @Controller('client')
-export class ClientController {}
+export class ClientController {
+
+}
